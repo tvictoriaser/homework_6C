e00eff54e7b2b2e5c84fd0d0ac14cdfa7b498263
Victoria <tikhomirovavs@gmail.com>
2022-12-06 19:45:33 +0300

    Закончили задания

---
+++ task41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите через пробел числа: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(),int.Parse)!;
+
+
+int NumbersMoreZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int numbersMoreZero = NumbersMoreZero(arr);
+PrintArray(arr);
+Console.WriteLine();
+Console.WriteLine($"Количество чисел больше 0 -> {numbersMoreZero}");
---
+++ task43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double PointOfIntersection(double a, double b, double c, double d)
+{
+    if ((a == b) && (c == d)) Console.Write("Прямые совпадают");
+    else if (a == b) Console.Write("Прямые параллельны");
+    else
+    {
+        double x = (d - c) / (a - b);
+        double y = a * x + c;
+        Console.Write($"Точка пересечения двух прямых -> ({x},{y})");
+    }
+    return 0;
+}
+
+PointOfIntersection(k1,k2,b1,b2);
